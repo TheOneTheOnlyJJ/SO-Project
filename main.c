542704f2e9a32ea68162bd6260619c5d7af5e80a
Jurj Andrei George <ajurj@cloudbasesolutions.com>
2023-12-05 16:19:58 +0200

    S6 done

--- main.c
+++ main.c
@@ -4,60 +4,98 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
-#define BMP_HEADER_WIDTH_OFFSET 18
-/// #define BMP_
+#include <string.h>
+#include <time.h>
 
 typedef struct {
-    int size;
-} BMPFileStats;
+    char numeFisier[BUFSIZ], drepturiUser[BUFSIZ], drepturiGrup[BUFSIZ], drepturiAltii[BUFSIZ];
+    int dimensiune, inaltime, lungime, userId, nrLegaturi;
+    time_t timpUltimaModificare;
+} ReqStats;
 
-static int readInt(int fd)
+static void getBMPFileStats(int in, char* filename, ReqStats* reqStats)
 {
-    /// TO BE IMPLEMENTED
-    return 0;
+    struct stat inStat;
+    int buffer;
+    /// Copy filename
+    strcpy(reqStats->numeFisier, filename);
+    /// Dimensiune
+    lseek(in, 18, SEEK_SET);
+    read(in, &buffer, 4);
+    reqStats->dimensiune = buffer;
+    /// Lungime
+    read(in, &buffer, 4);
+    reqStats->lungime = buffer;
+    /// Inaltime
+    read(in, &buffer, 4);
+    reqStats->inaltime = buffer;
+    /// Get other stats with fstat
+    fstat(in, &inStat);
+    reqStats->nrLegaturi = inStat.st_nlink;
+    reqStats->userId = inStat.st_uid;
+    reqStats->timpUltimaModificare = inStat.st_mtime;
+    /// Is dir
+    strcpy(reqStats->drepturiUser, S_ISDIR(inStat.st_mode) ? "d" : "-");
+    /// User rights
+    strcat(reqStats->drepturiUser, (inStat.st_mode & S_IRUSR) ? "r" : "-");
+    strcat(reqStats->drepturiUser, (inStat.st_mode & S_IWUSR) ? "w" : "-");
+    strcat(reqStats->drepturiUser, (inStat.st_mode & S_IXUSR) ? "x" : "-");
+    /// Group rights
+    strcat(reqStats->drepturiGrup, (inStat.st_mode & S_IRGRP) ? "r" : "-");
+    strcat(reqStats->drepturiGrup, (inStat.st_mode & S_IWGRP) ? "w" : "-");
+    strcat(reqStats->drepturiGrup, (inStat.st_mode & S_IXGRP) ? "x" : "-");
+    /// Others rights
+    strcat(reqStats->drepturiAltii, (inStat.st_mode & S_IROTH) ? "r" : "-");
+    strcat(reqStats->drepturiAltii, (inStat.st_mode & S_IWOTH) ? "w" : "-");
+    strcat(reqStats->drepturiAltii, (inStat.st_mode & S_IXOTH) ? "x" : "-");
+
+    return;
 }
 
-static BMPFileStats* getBMPFileStats(int fd)
+static int printfBMPFileStats(int out, ReqStats stats)
 {
-    /// Check file descriptor
-    if (fd < 0)
-    {
-        perror("Error reading input file");
-        return NULL;
-    }
-    /// Move cursor to read width & height
-    if (lseek(fd, BMP_HEADER_WIDTH_OFFSET, SEEK_SET) < 0)
-    {
-        perror("Error parsing file");
-        return NULL;
-    }
+    char buffer[BUFSIZ];
+    /// Nume fisier
+    sprintf(buffer, "Nume fisier: %s.\n", stats.numeFisier);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// Dimensiune
+    sprintf(buffer, "Dimensiune: %d.\n", stats.dimensiune);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// Lungime
+    sprintf(buffer, "Lungime: %d.\n", stats.lungime);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// Inaltime
+    sprintf(buffer, "Inaltime: %d.\n", stats.inaltime);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// User ID
+    sprintf(buffer, "User ID: %d.\n", stats.userId);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// Numar legaturi
+    sprintf(buffer, "Numar legaturi: %d.\n", stats.nrLegaturi);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// Last modified time
+    sprintf(buffer, "Timpul ultimei modificari: %li.\n", stats.timpUltimaModificare);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// Drepturi de acces user
+    sprintf(buffer, "Drepturi de acces user: %s.\n", stats.drepturiUser);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// Drepturi de acces grup
+    sprintf(buffer, "Drepturi de acces grup: %s.\n", stats.drepturiGrup);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+    /// Drepturi de acces altii
+    sprintf(buffer, "Drepturi de acces altii: %s.\n", stats.drepturiAltii);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
 
-    int width = 0;
-    int ret_val = read(fd, &width, sizeof(width));
-    if (ret_val < 0)
-    {
-        perror("Error reading file");
-        return NULL;
-    }
-    if (ret_val != sizeof(width))
-    {
-        fprintf(stderr, "Invalid bitmap file header.\n");
-        return NULL;
-    }
-
-    struct stat st;
-    if (fstat(fd, &st) < 0)
-    {
-        perror("Error reading stats");
-        return NULL;
-    }
-
-    return NULL;
-}
-
-static int printBMPFileStats()
-{
     return 0;
 }
 
@@ -66,14 +104,39 @@ int main(int argc, char **argv)
     /// Check args
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <input_file>.\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    fprintf(stdout, "Given file path : %s\n", argv[1]);
+    if (strstr(argv[1], ".bmp") == NULL)
+    {
+        fprintf(stderr, "Given argument does not point to a .bmp file : %s.\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stdout, "Given file path : %s.\n", argv[1]);
 
-    /// Open file
-    int fd = open(argv[1], O_RDONLY);
-    getBMPFileStats(fd);
+    /// Open required files
+    /// Input bmp file
+    int input_fd = open(argv[1], O_RDONLY);
+    if (input_fd == -1)
+    {
+        fprintf(stderr, "Could not open input file.\n");
+        exit(EXIT_FAILURE);
+    }
+    /// Output txt file
+    int output_fd = open("statistica.txt", O_WRONLY);
+    if (output_fd == -1)
+    {
+        fprintf(stderr, "Could not open output file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /// Get stats
+    ReqStats reqStats;
+    getBMPFileStats(input_fd, argv[1], &reqStats);
+    printfBMPFileStats(output_fd, reqStats);
+
+    close(input_fd);
+    close(output_fd);
 
     return 0;
 }
